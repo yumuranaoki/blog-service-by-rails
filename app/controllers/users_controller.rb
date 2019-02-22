15284b3d8821af35380e32cd432a95a3410e29f1
yumuranaoki <naoki.yumura@gmail.com>
2019-02-22 21:38:19 +0900

    create api for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def create
+    [name, email, password, password_confirmation] = params[:name], params[:email], params[:password], params[:password_confirmation]
+    @user = User.new(
+      name: name,
+      email: email,
+      password: password,
+      password_confirmation: password_confirmation
+    )
+
+    if @user.save
+      render json: {
+        'result': 'ok',
+        'userId': @user.id
+      }
+      session[:user_id] = @user.id
+    else
+      render json: {
+        'result': 'error',
+        'message': 'fail to register'
+      }
+    end
+  end
+
+  private
+
 end
